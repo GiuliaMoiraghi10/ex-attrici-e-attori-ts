d769fe66ebc2db03669520549e0bbd192e0591ad
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2025-05-09 17:58:25 +0200

    Milestone 3

--- src/main.ts
+++ src/main.ts
@@ -35,3 +35,47 @@ type Actress = Person & {
   awards: string;
   nationality: 'American' | 'British' | 'Australian' | 'Israeli-American' | 'South African' | 'French' | 'Indian' | 'Israeli' | 'Spanish' | 'South Korean' | 'Chinese.'
 }
+
+/* 📌 Milestone 3
+Crea una funzione getActress che, dato un id, effettua una chiamata a:
+
+GET https://boolean-spec-frontend.vercel.app/freetestapi/actresses/:id
+La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
+
+Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta. */
+
+function isActress(data: unknown): data is Actress {
+  return (
+    typeof data === 'object' && data !== null &&
+    'id' in data && typeof data.id === 'number' &&
+    'name' in data && typeof data.name === 'string' &&
+    'birth_year' in data && typeof data.birth_year === 'number' &&
+    'death_year' in data && typeof data.death_year === 'number' &&
+    'biography' in data && typeof data.biography === 'string' &&
+    'image' in data && typeof data.image === 'string' &&
+    'most_famous_movies' in data &&
+    data.most_famous_movies instanceof Array &&
+    data.most_famous_movies.length === 3 &&
+    data.most_famous_movies.every(movie => typeof movie === 'string') &&
+    'awards' in data && typeof data.awards === 'string' &&
+    'nationality' in data && typeof data.nationality === 'string'
+  )
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/actresses/${id}`)
+    const data: unknown = await response.json()
+    if (!isActress(data)) {
+      throw new Error('Dati non validi')
+    }
+    return data
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Errore durante la fetch:', error.message)
+    } else {
+      console.error('Errore sconosciuto:', error)
+    }
+    return null
+  }
+}
